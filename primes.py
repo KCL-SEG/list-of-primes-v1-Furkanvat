5796d6a419ede82437402c2aaf2fc3213333a206
Furkanvat <117448902+Furkanvat@users.noreply.github.com>
2023-09-24 18:37:02 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 2
+    def isPrime(n):
+        if n == 1:
+            return True
+        for i in range(2,n):
+            if (n % i) == 0:
+                return False
+        
+        return True
+            
+    while len(list) < number_of_primes:
+        if isPrime(counter):
+            list.append(counter)
+        counter += 1
+
     return list
